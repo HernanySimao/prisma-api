ebdc9f8f990d2be2b3c8871572afb558a1a95a3f
Hernany Simao <hernanysimao123@gmail.com>
2024-08-20 16:42:08 +0100

    Create services

---
+++ src/users/repositories/users.repository.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
+import { UserEntity } from "../entities/user.entity";
+
+@Injectable()
+
+export class UsersRepository{
+
+    constructor(private readonly prisma: PrismaService){}
+
+    async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+       return this.prisma.user.create({
+        data: createUserDto
+       })
+      }
+    
+     async findAll(): Promise<UserEntity[]> {
+        return await this.prisma.user.findMany()
+      }
+    
+      findOne(id: number): Promise<UserEntity>{
+      return this.prisma.user.findUnique({ 
+        where: {
+            id
+        }
+      })
+      }
+    
+    async  update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+        return this.prisma.user.update({
+            where: {
+                id
+            },
+            data: updateUserDto
+        })
+      }
+    
+      remove(id: number) {
+        return `This action removes a #${id} user`;
+      }
+}
